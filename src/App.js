bde8e990bf171b42ed84b6ddbd662cab289383cf
jasperalen07 <125176411+jasperalen07@users.noreply.github.com>
2024-01-26 16:08:59 -0600

    tested the sass styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-
+import './scss/styles.scss'
 function App() {
   return (
     <div className="App">
