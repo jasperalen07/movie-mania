a3d6f64c052c786ee278c3ab07a5da117f703434
jasperalen07 <125176411+jasperalen07@users.noreply.github.com>
2024-01-31 22:00:38 -0600

    took the title and release paragraph

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,8 +3,8 @@ function MovieCard({movie}){
     return(
         
             <div key={movie.id} className="movie-card">
-                <p>Title: {movie.original_title}</p>
-                <p>Release Date: {movie.release_date}</p>
+                <p> {movie.original_title}</p>
+                <p> {movie.release_date}</p>
                 
                 <img
                             src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
