925faa1c57d8d514e62a156b990e5d998512de68
jasperalen07 <125176411+jasperalen07@users.noreply.github.com>
2024-02-01 13:54:34 -0600

    search component started

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,24 +1,39 @@
 import React, { useState } from 'react';
 
+
+
 function Search() {
   const [query, setQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Submitted Query', query);
+  };
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
   
+return (
 
-
-
-<form action='/search'
+<form 
  method='get' 
  className='search_form'
  id='search-input'
  value='search'
  onSubmit={handleSubmit}
  > 
-<label className='search'  > Search a movie</label>
+<label className='search'  > Search </label>
 <input type='text' id='search-input' name='search'  />
-
+ <button type="submit">Search</button>
 
  
 </form>
+)
+
   
+
+}
+
+export default Search
