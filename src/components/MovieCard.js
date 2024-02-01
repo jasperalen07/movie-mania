f498aa7e5857ea5d6cc5630de13ed71e52a704b0
jasperalen07 <125176411+jasperalen07@users.noreply.github.com>
2024-02-01 12:40:26 -0600

    added vote average

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { formatReleaseDate } from "../utilities/toolbelt";
 function MovieCard({movie}){
     const navigate = useNavigate();
     return(
@@ -10,14 +11,14 @@ function MovieCard({movie}){
                 navigate(`/movie/${movie.id}`);
               }} >
                 <p> {movie.original_title}</p>
-                <p> {movie.release_date}</p>
+                <p>{formatReleaseDate(movie.release_date)}</p>
+                <p>{movie.vote_average}</p>
                 
                 <img
                             src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
                             alt={`Backdrop for ${movie.original_title}`}
                             style={{ maxWidth: "80%" }}
-                        />
-
+                        /> 
             </div>
         
     );
